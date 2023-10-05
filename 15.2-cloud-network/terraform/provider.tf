c40fdfe5d45821deb3128ba114b601cefd8d4d7d
awertoss <109097833+awertoss@users.noreply.github.com>
2023-10-05 13:39:02 +0300

    Create provider.tf

---
+++ 15.2-cloud-network/terraform/provider.tf
@@ -0,0 +1,6 @@
+provider "yandex" {
+ token     = "y0_AgAAAAAFc1G************1wUSQXef1ejzte5OnZFU6qQ".
+ cloud_id  = var.yandex_cloud_id
+ folder_id = var.yandex_folder_id
+ zone      = var.b-zone
+}
