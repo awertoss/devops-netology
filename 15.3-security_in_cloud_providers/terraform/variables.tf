b2fce7da4d7faf5bde6a04e6b05462ed9a1cbcc9
awertoss <109097833+awertoss@users.noreply.github.com>
2023-10-15 17:44:05 +0300

    Create variables.tf

---
+++ 15.3-security_in_cloud_providers/terraform/variables.tf
@@ -0,0 +1,17 @@
+]
+variable "yandex_cloud_id" {
+  default = "b1gnj******j34hrh7s"
+}
+
+variable "yandex_folder_id" {
+  default = "b1gd02******7v2h14"
+}
+
+variable "b-zone" {
+  default = "ru-central1-b"
+}
+
+variable "lamp-instance-image-id" {
+  default = "fd827b91d99psvq5fjit"
+}
+
