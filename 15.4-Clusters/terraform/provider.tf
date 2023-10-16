4bfaa3bcb0223168778d19137d2a1839a586f2f4
awertoss <109097833+awertoss@users.noreply.github.com>
2023-10-16 18:38:38 +0300

    Create provider.tf

---
+++ 15.4-Clusters/terraform/provider.tf
@@ -0,0 +1,17 @@
+]
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.78.2"
+    }
+  }
+}
+
+provider "yandex" {
+  token = "y0_AgAAAAAFc18888*******USQXef1ejzte5OnZFU6qQ"
+  # service_account_key_file = "key.json"
+  cloud_id  = "b1gnj*****hrh7s"
+  folder_id = "b1gd*****h14"
+  zone      = var.yandex_compute_default_zone
+}
